432fb8bf74d435d9922f79e5f058ff1ea253acec
Ekaterina Krutakova <lagoonnw@gmail.com>
2020-09-03 17:38:41 +0500

    docker

---
+++ api/src/index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const {connectDb} = require('./helpers/db');
+const {host, port, db} = require('./configuration');
+const postSchema = new mongoose.Schema({
+  name: String
+});
+const Post = mongoose.model('Post', postSchema);
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started api on port ${port}`);
+    console.log(`Started api on host ${host}`);
+    console.log(`Our database: ${db}`);
+    
+    Post.find((err, posts) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(posts);
+    });
+    const silence = new Post({name: 'Silence'});
+    silence.save((err, savedSilence) => {
+      if(err) {
+        console.log(err);
+      }
+      console.log('savedSilence: ', savedSilence);
+    });
+    console.log(silence.name);
+  });
+};
+app.get('/test', (req, res) => {
+  res.send('API WORKS!')
+});
+
+connectDb()
+  .on('error', console.log)
+  .on('disconnected', connectDb)
+  .once('open', startServer);
---
+++ auth/src/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const {connectDb} = require('./helpers/db');
+const {host, port, db} = require('./configuration');
+
+const startServer = () => {
+  app.listen(port, () => {
+    console.log(`Started auth on port ${port}`);
+    console.log(`Started auth on host ${host}`);
+    console.log(`Our auth database: ${db}`);
+  });
+};
+app.get('/test', (req, res) => {
+  res.send('AUTH WORKS!')
+});
+
+connectDb()
+  .on('error', console.log)
+  .on('disconnected', connectDb)
+  .once('open', startServer);
