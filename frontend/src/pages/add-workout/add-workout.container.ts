fef970feb43676b5aea42e64eef161342eecdb3f
Krutakova E <krutakova@useit.pro>
2021-05-24 16:49:45 +0500

    Remove unused code

--- frontend/src/pages/add-workout/add-workout.container.ts
+++ frontend/src/pages/add-workout/add-workout.container.ts
@@ -2,7 +2,6 @@ import {connect}                        from "react-redux";
 import { bindActionCreators, Dispatch } from "redux";
 import {addWorkout}                     from '../../store/actions/workouts';
 import { AddWorkoutComponent }          from "./add-workout.component";
-import { AppState }                     from "../../store/reducers/root.reducer";
 
 const mapStateToProps = (state: any, ownProps: any) => ({
   ...state,
--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,26 +1,5 @@
 import axios          from 'axios';
-import {BASE_API_URL, ResponseStatus} from "../constants/constants";
-
-//
-//
-// export const createAPI = () => {
-//   const api = axios.create({
-//     baseURL: BASE_API_URL,
-//     timeout: 1000 * 5,
-//     withCredentials: true,
-//   });
-//
-//   // const onSuccess = (response: any) => response;
-//   //
-//   // const onFail = (err: any) => {
-//   //
-//   //   return err;
-//   // };
-//   //
-//   // api.interceptors.response.use(onSuccess, onFail);
-//
-//   return api;
-// };
+import {BASE_API_URL} from "../constants/constants";
 
 export const api = axios.create({
   baseURL: BASE_API_URL,
